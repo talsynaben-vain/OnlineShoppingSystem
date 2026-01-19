a59e7a781ab524363ba77ebf1c3031db48d7d5b2
Talshyn <talsynaben@gmail.com>
2026-01-19 15:21:34 +0600

    Add JDBC PostgreSQL CRUD operations

---
+++ src/DBConnection.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/online_shopping";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "Postgres123";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
